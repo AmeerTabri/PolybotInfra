99d17baa7f2c1c2415fe25dbec7f7fc512861d0e
ameertabri <ameer.t.2000@gmail.com>
2025-06-24 01:41:49 +0300

    cp ec2

---
+++ tf/region.us-west-2.tfvars
@@ -0,0 +1,2 @@
+region  = "us-west-2"
+ami_id  = "ami-05f991c49d264708f"
---
+++ tf/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "ami-05f991c49d264708f"
+  type        = string
+}
